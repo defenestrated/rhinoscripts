4da06421216dd136ccd524b4a9cca705aba9c5b5
sam <sam@D-1817B-14726.tns.newschool.edu>
2017-05-01 13:41:27 -0400

    structure

---
+++ drawnormals.py
@@ -0,0 +1,32 @@
+# use rhinoscriptsyntax to get all the functions in one shot
+import rhinoscriptsyntax as rs
+import random
+import time
+import Rhino
+
+# groups = rs.SelectedObjects()
+obj = rs.SelectedObjects()
+# exaggerate = rs.GetReal("exaggeration multiplier (try it first and then adjust, default is 1.0)")
+# repeat = rs.GetInteger("number of times to repeat (skip for single execution)")
+
+print(obj)
+
+# if not repeat:
+# repeat = 20
+# exaggerate = 0.02
+
+# # if not exaggerate:
+# #     exaggerate = 1.0
+# #     print("no dice")
+# #     print(exaggerate)
+# # else:
+# #     print("multiplier supplied")
+# #     print(exaggerate)
+
+# print("exaggeration: ", exaggerate, " :: repeating ", repeat, " times")
+
+# for time in range(repeat):
+#     for pt in points:
+#         translation = [(random.random()-0.5)*exaggerate, (random.random()-0.5)*exaggerate, (random.random()-0.5)*exaggerate]
+#         rs.MoveObject(pt, translation)
+#     rs.AddInterpCurve(points)
